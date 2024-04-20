31b731cd26698e965492d61ed408b74153a4c0bb
archit203 <archit241001@gmail.com>
2024-04-20 13:29:52 +0530

    signup route created

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
+
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string
@@ -10,13 +11,29 @@ const app = new Hono<{
 
 
 
-app.post('/api/v1/user/signup', (c) => {
-  
+app.post('/api/v1/user/signup', async (c) => {
+
+  const body = await c.req.json()
+
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate())
 
-  return c.text('Hello World')
+  try {
+    // duplicate email
+    await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password,
+        name: body.name
+      }
+    })
+  }catch(e){
+    c.status(403);
+    return c.text('User already exists')
+  }
+
+  return c.text('sign-up successful')
 });
 
 app.post('/api/v1/user/signin', (c) => {
